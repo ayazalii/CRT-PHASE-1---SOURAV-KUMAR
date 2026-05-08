f19815ce85a909b38f91a8e8761dec952b68b3d8
ayazalii <ayazali935915@gmail.com>
2026-05-08 10:35:02 +0530

    Normal

---
+++ Day 8/encapsulation.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class ATM{
+    private:
+    int balance;
+
+    public:
+//ENCAPSULATION
+    void setbalance (int balance){
+        this->balance=balance;
+    }
+    //WITHDRAW FROM ATM
+
+    void withdraw(int amount){
+        if(amount>balance){
+            cout<<"Insufficient balance."<<endl;
+        }else{
+            balance-=amount;
+        cout<<"Amount withdrawn: "<<amount<<endl;
+
+        }
+        
+    }
+
+    //SHOW BALANCE METHOD OF ATM
+    int getbalance(){
+        return balance;
+    }
+
+};
+int main(){
+    int n;
+    cin>>n;
+    ATM a;
+    a.setbalance(n);
+    cout<<"Balance is : "<<a.getbalance()<<endl;
+    a.withdraw(500);
+    cout<<"Balance is: "<<a.getbalance()<<endl;
+    return 0;
+}
